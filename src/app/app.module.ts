5c4001038620683c17ed2587a302c7311d068e36
Theary Im and Young Liu <young@>
2019-04-10 14:23:24 -0700

    add ability to search for mars rover photos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from "@angular/http"
+import { routing } from "./app.routing";
+import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
+import { RoverFormComponent } from './rover-form/rover-form.component';
+import { PhotosListComponent } from './photos-list/photos-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoverFormComponent,
+    PhotosListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    routing,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/rover-form/rover-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { MarsRoverApiPhotosService } from '../mars-rover-api-photos.service';
+
+@Component({
+  selector: 'app-rover-form',
+  templateUrl: './rover-form.component.html',
+  styleUrls: ['./rover-form.component.css'],
+  providers: [MarsRoverApiPhotosService]
+})
+export class RoverFormComponent implements OnInit {
+  photos: any[] = null;
+
+  constructor(private marsRoverPhotos: MarsRoverApiPhotosService) { }
+  getRoverImages(date: string, camera: string){
+    console.log("aaaaaaaaaaaaa");
+    this.marsRoverPhotos.getByDateAndCamera(date, camera).subscribe(response => {
+      this.photos = response.json();
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
